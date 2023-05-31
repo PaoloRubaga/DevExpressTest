910454955d59a2a9aa04024f31ea5db5cd2ffeda
PaoloRubaga <paolo.rubaga@geeo.it>
2023-05-31 13:00:22 +0200

    fine modifiche form e filtri

--- DevExpressTest/Data/WeatherForecast.cs
+++ DevExpressTest/Data/WeatherForecast.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace DevExpressTest.Data
 {
     public class WeatherForecast
@@ -10,6 +8,8 @@ namespace DevExpressTest.Data
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public List<string> Summary { get; set; }
+
+        public string SummaryString => string.Join(",", Summary);
     }
 }
--- DevExpressTest/Data/WeatherForecastService.cs
+++ DevExpressTest/Data/WeatherForecastService.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace DevExpressTest.Data
 {
     public class WeatherForecastService
     {
-        private static readonly string[] Summaries = new[]
+        public static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
@@ -18,7 +14,7 @@ namespace DevExpressTest.Data
             {
                 Date = startDate.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = new List<string>() { Summaries[rng.Next(Summaries.Length - 1)], Summaries[rng.Next(Summaries.Length - 1) + 1] }
             }).ToArray());
         }
     }
